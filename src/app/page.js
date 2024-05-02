0602d7cb9c0274469b656122ce0a87a6be6ec266
robertSouDev <rogerhallks1@gmail.com>
2024-05-02 15:54:57 -0300

    Form

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
+'user client'
 import Header from "./components/Header/Header";
+import LoginForm from "./components/Header/LoginForm";
 
 export default function Home() {
   return (
     <main className="">
         <div>
             <Header/>
+            <LoginForm/>
         </div>
     </main>
   );
