51997668003335bbc29aa8aad2fed04d91e4f9f7
robertSouDev <rogerhallks1@gmail.com>
2024-05-02 16:22:19 -0300

    formulario de login funcionando

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,13 +1,19 @@
+'use client';
+
+import { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { AiOutlinePlus } from "react-icons/ai";
 
+
 export default function Header() {
+    const [show,setShow] = useState(false)
     return (
         <div className="flex justify-center bg-green-500 items-center px-1 h-full py-4">
         <header className="flex space-x-64 gap-x-32">
             <h1 className="text-center">Cat List</h1>
             <button> + Add Cat</button>
-            <button><AiOutlineUser /></button>
+            <button onClick={()=> setShow(true)}><AiOutlineUser /></button>
+            {/* <Modal show={show}/> */}
         </header>
         </div>
     )
--- src/app/components/Header/LoginForm.jsx
+++ src/app/components/Header/LoginForm.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
+import apiGatos from '../../services/apiService.jsx';
 
 const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await apiGatos.logarConta(formData);
+      console.log('Resposta da API:', response);
+      // Faça algo com a resposta, como redirecionar para outra página ou exibir uma mensagem de sucesso
+    } catch (error) {
+      console.error('Erro ao enviar formulário:', error);
+      // Trate o erro, como exibir uma mensagem de erro para o usuário
+    }
+  };
+
   return (
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-          Nome
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
-          type="text"
-          placeholder="Nome"
-        />
-      </div>
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
       <div className="mb-6">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
           E-mail
@@ -23,6 +37,8 @@ const LoginForm = () => {
           id="email"
           type="email"
           placeholder="E-mail"
+          value={formData.email}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-6">
@@ -34,6 +50,8 @@ const LoginForm = () => {
           id="password"
           type="password"
           placeholder="Senha"
+          value={formData.password}
+          onChange={handleChange}
         />
       </div>
       <div className="flex items-center justify-between">
