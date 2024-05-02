1ae2d7b0157df6528bb553ff23c9dc8979c915bd
juliana <juliana.camposrodrigues7@gmail.com>
2024-05-02 16:00:57 -0300

    Implementando services

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Header from "./components/Header/Header";
+import Form from "./components/formularios/Form";
 
 export default function Home() {
   return (
     <main className="">
         <div>
             <Header/>
+            <Form />
         </div>
     </main>
   );
