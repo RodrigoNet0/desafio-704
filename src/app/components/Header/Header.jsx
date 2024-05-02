9f3abae88342e0248b7d96e2285820f6294490cb
FRANCISCO RODRIGUES SOBRINHO NETO <FRANCISCO6037@DIGITALCOLLEGE.LOCAL>
2024-05-02 15:00:30 -0300

    fix: bug

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Cat List</h1>
+            <button>Add Cat</button>
+            <button>icon</button>
+        </header>
+    )
+}
